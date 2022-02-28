7bb9b966024da5f657a9989b8b62d63486b3a3ec
bendn <bend.n@outlook.com>
2022-02-28 19:46:33 +0700

    yayyyy totally working
    (not)

--- leveldata.gd
+++ leveldata.gd
@@ -1,4 +1,7 @@
 extends Node
 
 export(PoolColorArray) var dot_colors
-export(Color) var background_collor
+export(Color) var background_color
+
+func _ready():
+	VisualServer.set_default_clear_color(background_color)
